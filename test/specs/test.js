044c191723b6473134672f0dff0103608291bab7
dewicharolinas <dewicharolina23@gmail.com>
2024-05-02 19:53:03 +0700

    menambahkan cart button

--- test/specs/test.js
+++ test/specs/test.js
@@ -8,6 +8,7 @@ describe('Test Case', () => {
         const username = await browser.$('#user-name')
         const  password = await browser.$('#password')
         const loginButton = await browser.$('#login-button')
+        const cartButon = await browser.$('#cart-button')
 
 
         await username.setValue('standard_user')
@@ -17,20 +18,20 @@ describe('Test Case', () => {
 
         await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html')  
         await browser.pause(5000)
-
-        // const backpakCartButton = await $('#add-to-cart-sauce-labs-backpack')
-        // await expect(backpakCartButton).toBeDisplayed()
-        // await backpakCartButton.click()
         
         const lightbikeCartButton = await $('#add-to-cart-sauce-labs-bike-light')
         await expect(lightbikeCartButton).toBeDisplayed()
         await lightbikeCartButton.click()
         
         const cartBadge = await $('//*[@id="shopping_cart_container"]/a/span')
-       
-        
         await expect(cartBadge).toBeDisplayed()
         await expect(cartBadge).toHaveText('1')
+
+        const cartButton = await $('//*[@id="shopping_cart_container"]/a')
+        console.log (`Value dari button ${ await cartButton.getValue()}`)
+        await cartButton.click()
+
+
         
         await browser.pause(10000)
        
